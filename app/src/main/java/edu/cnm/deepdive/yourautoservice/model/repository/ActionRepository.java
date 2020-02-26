2cbe4eb0ec5c234b626ad930ca826b3505af229a
Richmond <rbaafi@cnm.edu>
2020-02-25 17:13:26 -0700

    Created Repositories.

--- app/src/main/java/edu/cnm/deepdive/yourautoservice/model/repository/ActionRepository.java
+++ app/src/main/java/edu/cnm/deepdive/yourautoservice/model/repository/ActionRepository.java
@@ -1,5 +1,30 @@
 package edu.cnm.deepdive.yourautoservice.model.repository;
 
+import android.app.Application;
+import edu.cnm.deepdive.yourautoservice.service.VehicleDatabase;
+
 public class ActionRepository {
+
+  private final VehicleDatabase database;
+  private static Application context;
+  private ActionRepository() {
+    if (context == null) {
+      throw new IllegalStateException();
+    }
+    database = VehicleDatabase.getInstance();
+  }
+  public VehicleDatabase getDatabase() {
+    return database;
+  }
+  public static Application getContext() {
+    return context;
+  }
+  public static void setContext(Application context) {
+    ActionRepository.context = context;
+  }
+  private static class InstanceHolder {
+    private static final ActionRepository INSTANCE = new ActionRepository();
+  }
+
 
 }
--- app/src/main/java/edu/cnm/deepdive/yourautoservice/model/repository/CarRepository.java
+++ app/src/main/java/edu/cnm/deepdive/yourautoservice/model/repository/CarRepository.java
@@ -1,5 +1,29 @@
 package edu.cnm.deepdive.yourautoservice.model.repository;
 
+import android.app.Application;
+import edu.cnm.deepdive.yourautoservice.service.VehicleDatabase;
+
 public class CarRepository {
+
+  private final VehicleDatabase database;
+  private static Application context;
+  private CarRepository() {
+    if (context == null) {
+      throw new IllegalStateException();
+    }
+    database = VehicleDatabase.getInstance();
+  }
+  public VehicleDatabase getDatabase() {
+    return database;
+  }
+  public static Application getContext() {
+    return context;
+  }
+  public static void setContext(Application context) {
+    CarRepository.context = context;
+  }
+  private static class InstanceHolder {
+    private static final CarRepository INSTANCE = new CarRepository();
+  }
 
 }
