b3a2e2f8775e8333bf8afa45b43f389ca41b4785
Richmond <rbaafi@cnm.edu>
2020-02-25 17:58:51 -0700

    Created Repositories.

--- app/src/main/java/edu/cnm/deepdive/yourautoservice/model/repository/AvailableCarRepository.java
+++ app/src/main/java/edu/cnm/deepdive/yourautoservice/model/repository/AvailableCarRepository.java
@@ -1,5 +1,31 @@
 package edu.cnm.deepdive.yourautoservice.model.repository;
 
+import android.app.Application;
+import edu.cnm.deepdive.yourautoservice.service.VehicleDatabase;
+
 public class AvailableCarRepository {
+
+
+  private final VehicleDatabase database;
+  private static Application context;
+  private AvailableCarRepository() {
+    if (context == null) {
+      throw new IllegalStateException();
+    }
+    database = VehicleDatabase.getInstance();
+  }
+  public VehicleDatabase getDatabase() {
+    return database;
+  }
+  public static Application getContext() {
+    return context;
+  }
+  public static void setContext(Application context) {
+    AvailableCarRepository.context = context;
+  }
+  private static class InstanceHolder {
+    private static final AvailableCarRepository INSTANCE = new AvailableCarRepository();
+  }
+
 
 }
--- app/src/main/java/edu/cnm/deepdive/yourautoservice/model/repository/ServiceRepository.java
+++ app/src/main/java/edu/cnm/deepdive/yourautoservice/model/repository/ServiceRepository.java
@@ -1,5 +1,31 @@
 package edu.cnm.deepdive.yourautoservice.model.repository;
 
+import android.app.Application;
+import edu.cnm.deepdive.yourautoservice.service.VehicleDatabase;
+
 public class ServiceRepository {
+
+
+  private final VehicleDatabase database;
+  private static Application context;
+  private ServiceRepository() {
+    if (context == null) {
+      throw new IllegalStateException();
+    }
+    database = VehicleDatabase.getInstance();
+  }
+  public VehicleDatabase getDatabase() {
+    return database;
+  }
+  public static Application getContext() {
+    return context;
+  }
+  public static void setContext(Application context) {
+    ServiceRepository.context = context;
+  }
+  private static class InstanceHolder {
+    private static final ServiceRepository INSTANCE = new ServiceRepository();
+  }
+
 
 }
