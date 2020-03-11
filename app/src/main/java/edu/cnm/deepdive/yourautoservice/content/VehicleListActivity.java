9281a65d8378b0edeeec0ece0ec251e24082e28d
Richmond <rbaafi@cnm.edu>
2020-03-11 14:09:34 -0600

    Getting rid of dummy content from masterDetail layout.

--- app/src/main/java/edu/cnm/deepdive/yourautoservice/content/VehicleListActivity.java
+++ app/src/main/java/edu/cnm/deepdive/yourautoservice/content/VehicleListActivity.java
@@ -1,7 +1,5 @@
 package edu.cnm.deepdive.yourautoservice.content;
 
-import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,14 +7,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import androidx.appcompat.widget.Toolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 import edu.cnm.deepdive.yourautoservice.R;
 
-import edu.cnm.deepdive.yourautoservice.content.CarContent.CarItem;
-import java.util.List;
+import edu.cnm.deepdive.yourautoservice.view.VehicleRecyclerAdapter;
 
 /**
  * An activity representing a list of Vehicles. This activity has different presentations for
@@ -63,76 +57,7 @@ public class VehicleListActivity extends AppCompatActivity {
   }
 
   private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
-    recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(this, CarContent.ITEMS, mTwoPane));
+    recyclerView.setAdapter(new VehicleRecyclerAdapter(this, CarContent.ITEMS, mTwoPane));
   }
 
-  public static class SimpleItemRecyclerViewAdapter
-      extends RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder> {
-
-    private final VehicleListActivity mParentActivity;
-    private final List<CarItem> mValues;
-    private final boolean mTwoPane;
-    private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        CarItem item = (CarItem) view.getTag();
-        if (mTwoPane) {
-          Bundle arguments = new Bundle();
-          arguments.putString(VehicleDetailFragment.ARG_ITEM_ID, item.id);
-          VehicleDetailFragment fragment = new VehicleDetailFragment();
-          fragment.setArguments(arguments);
-          mParentActivity.getSupportFragmentManager().beginTransaction()
-              .replace(R.id.vehicle_detail_container, fragment)
-              .commit();
-        } else {
-          Context context = view.getContext();
-          Intent intent = new Intent(context, VehicleDetailActivity.class);
-          intent.putExtra(VehicleDetailFragment.ARG_ITEM_ID, item.id);
-
-          context.startActivity(intent);
-        }
-      }
-    };
-
-    SimpleItemRecyclerViewAdapter(VehicleListActivity parent,
-        List<CarItem> items,
-        boolean twoPane) {
-      mValues = items;
-      mParentActivity = parent;
-      mTwoPane = twoPane;
-    }
-
-    @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-      View view = LayoutInflater.from(parent.getContext())
-          .inflate(R.layout.vehicle_list_content, parent, false);
-      return new ViewHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
-      holder.mIdView.setText(mValues.get(position).id);
-      holder.mContentView.setText(mValues.get(position).content);
-
-      holder.itemView.setTag(mValues.get(position));
-      holder.itemView.setOnClickListener(mOnClickListener);
-    }
-
-    @Override
-    public int getItemCount() {
-      return mValues.size();
-    }
-
-    class ViewHolder extends RecyclerView.ViewHolder {
-
-      final TextView mIdView;
-      final TextView mContentView;
-
-      ViewHolder(View view) {
-        super(view);
-        mIdView = (TextView) view.findViewById(R.id.id_text);
-        mContentView = (TextView) view.findViewById(R.id.content);
-      }
-    }
-  }
 }
---
+++ app/src/main/java/edu/cnm/deepdive/yourautoservice/view/VehicleRecyclerAdapter.java
@@ -0,0 +1,100 @@
+package edu.cnm.deepdive.yourautoservice.view;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import edu.cnm.deepdive.yourautoservice.R;
+import edu.cnm.deepdive.yourautoservice.content.CarContent.CarItem;
+import edu.cnm.deepdive.yourautoservice.content.VehicleDetailActivity;
+import edu.cnm.deepdive.yourautoservice.content.VehicleDetailFragment;
+import edu.cnm.deepdive.yourautoservice.content.VehicleListActivity;
+import edu.cnm.deepdive.yourautoservice.model.entity.Car;
+import java.text.DateFormat;
+import java.util.List;
+
+public class VehicleRecyclerAdapter
+    extends RecyclerView.Adapter<VehicleRecyclerAdapter.ViewHolder> {
+
+  private final VehicleListActivity mParentActivity;
+  private final List<Car> cars;
+  private final DateFormat dateFormat;
+  private final boolean mTwoPane;
+  private final View.OnClickListener mOnClickListener = new View.OnClickListener() {
+    @Override
+    public void onClick(View view) {
+      CarItem item = (CarItem) view.getTag();
+      if (mTwoPane) {
+        Bundle arguments = new Bundle();
+        arguments.putString(VehicleDetailFragment.ARG_ITEM_ID, item.id);
+        VehicleDetailFragment fragment = new VehicleDetailFragment();
+        fragment.setArguments(arguments);
+        mParentActivity.getSupportFragmentManager().beginTransaction()
+            .replace(R.id.vehicle_detail_container, fragment)
+            .commit();
+      } else {
+        Context context = view.getContext();
+        Intent intent = new Intent(context, VehicleDetailActivity.class);
+        intent.putExtra(VehicleDetailFragment.ARG_ITEM_ID, item.id);
+
+        context.startActivity(intent);
+      }
+    }
+  };
+
+  public VehicleRecyclerAdapter(VehicleListActivity parent,
+      List<Car> items,
+      boolean twoPane) {
+    cars = items;
+    mParentActivity = parent;
+    mTwoPane = twoPane;
+    dateFormat = android.text.format.DateFormat.getMediumDateFormat(parent);
+  }
+
+  @Override
+  public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    View view = LayoutInflater.from(parent.getContext())
+        .inflate(R.layout.vehicle_list_content, parent, false);
+    return new ViewHolder(view, dateFormat);
+  }
+
+  @Override
+  public void onBindViewHolder(final ViewHolder holder, int position) {
+    holder.bind(position, cars.get(position));
+  }
+
+  @Override
+  public int getItemCount() {
+    return cars.size();
+  }
+
+  class ViewHolder extends RecyclerView.ViewHolder {
+
+    final TextView make;
+    final TextView model;
+    final TextView year;
+    final TextView date;
+    final DateFormat format;
+
+    private ViewHolder(View view, DateFormat format) {
+      super(view);
+      make = (TextView) view.findViewById(R.id.make);
+      model = (TextView) view.findViewById(R.id.model);
+      year = (TextView) view.findViewById(R.id.year);
+      date = (TextView) view.findViewById(R.id.date);
+      this.format = format;
+    }
+
+    private void bind(int position, Car car) {
+      make.setText(car.getMake());
+      model.setText(car.getModel());
+      year.setText(Integer.toString(car.getYear()));
+      date.setText(format.format(car.getAcquisition()));
+    }
+
+  }
+}
